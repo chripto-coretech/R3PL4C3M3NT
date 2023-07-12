2f769baf21e009af4dccbc10ae592d354bb1986d
Christian Carelli <67265231+chripto-coretech@users.noreply.github.com>
2023-07-12 23:38:37 +0200

    Write fix...

--- code.php
+++ code.php
@@ -5,7 +5,7 @@ function r3pl4c3m3nt($text){
    $normalChar = ['A', 'B', 'E', 'I', 'O', 'S', 'T'];
    $r3pl4c3Char = ['4', '8', '3', '1', '0', '5', '7'];
    
-   $text = str_ireplace($$normalChar, $r3pl4c3Char, $text);
+   $text = str_ireplace($normalChar, $r3pl4c3Char, $text);
    
    return $text;
    
