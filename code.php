6b22b38a5424e62d6d569007c4a19ec0c3b13515
Christian Carelli <67265231+chripto-coretech@users.noreply.github.com>
2023-07-12 23:32:07 +0200

    Define chars array

--- code.php
+++ code.php
@@ -1,8 +1,14 @@
 <?php
+
 function r3pl4c3m3nt($text){
-   $normalChar = [];
-   $r3pl4c3Char = [];
+   
+   $normalChar = ['A', 'B', 'E', 'I', 'O', 'S', 'T'];
+   $r3pl4c3Char = ['4', '8', '3', '1', '0', '5', '7'];
+   
    $text = str_replace($$normalChar, $r3pl4c3Char, $text);
+   
    return $text;
+   
 }
+
 ?>
